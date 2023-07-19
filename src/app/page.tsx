03fbdca149295fdd64217f158cfdbb210be4405f
czenturion <czenturijon@gmail.com>
2023-07-19 16:29:32 +0300

    v0.0.4 | rtk api in page.tsx usage complete.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,14 @@
 "use client";
 
-import Image from 'next/image'
-import styles from './page.module.css'
+import { useGetUsersQuery } from "@/redux/services/userApi";
 import { decrement, increment, reset } from "@/redux/features/counterSlice";
 import { useAppDispatch, useAppSelector } from "@/redux/hook";
 
 export default function Home() {
     const count = useAppSelector((state) => state.counterReducer.value);
     const dispatch = useAppDispatch();
+
+    const { isLoading, isFetching, data, error } = useGetUsersQuery(null);
 
     return (
         <main style={{maxWidth: 1200, marginInline: "auto", padding: 20}}>
@@ -22,6 +23,46 @@ export default function Home() {
                 </button>
                 <button onClick={() => dispatch(reset())}>reset</button>
             </div>
+            {
+                error ? (
+                    <p>Oh no, there was an error</p>
+                ) : isLoading || isFetching ? (
+                    <p>Loading...</p>
+                ) : data ? (
+                    <div
+                        style={{
+                            display: "grid",
+                            gridTemplateColumns: "1fr 1fr 1fr 1fr",
+                            gap: "20px",
+                        }}
+                    >
+                        {
+                            data.map((user) => (
+                                <div
+                                    key={user.id}
+                                    style={{
+                                        border: "1px solid #ccc",
+                                        textAlign: "center",
+                                    }}
+                                >
+                                    <img
+                                        src={`https://robohash.org/${user.id}?set=set2&size=180x180`}
+                                        alt={user.name}
+                                        style={{
+                                            height: 180,
+                                            width: 180
+                                        }}
+                                    />
+                                    <h3>
+                                        {user.name}
+                                    </h3>
+                                </div>
+                                )
+                            )
+                        }
+                    </div>
+                ) : null
+            }
         </main>
     )
 }
